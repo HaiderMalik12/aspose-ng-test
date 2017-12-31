831959520b2bac0d3de0c312ab514a22d81f3ddf
HaiderMalik <haidermalik504@gmail.com>
2017-12-31 13:43:35 +0500

    created apijs

---
+++ server/api.js
@@ -0,0 +1,19 @@
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+const config = require('./config');
+
+module.exports = function(app, config) {
+
+  // Authentication middleware
+  const jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `https://${config.AUTH0_DOMAIN}/.well-known/jwks.json`
+    }),
+    audience: config.AUTH0_API_AUDIENCE,
+    issuer: `https://${config.AUTH0_DOMAIN}/`,
+    algorithm: 'RS256'
+  });
+};
