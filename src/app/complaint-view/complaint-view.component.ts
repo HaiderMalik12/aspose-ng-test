fe185f55e17cec5ef17af9da439ceedd096ecd3f
HaiderMalik <haidermalik504@gmail.com>
2018-01-02 09:01:59 +0500

    added complaint-view componenet

---
+++ src/app/complaint-view/complaint-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-complaint-view',
+  template: '<formio src="https://bdtpdgjewsrxied.form.io/complaint"></formio>',
+  styleUrls: ['./complaint-view.component.css']
+})
+export class ComplaintViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
